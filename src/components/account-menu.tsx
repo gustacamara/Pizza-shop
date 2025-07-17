5c33b56d64aaac19e2d3b79bf2395d80aded007c
gustacamara <gustacamara.dev@hotmail.com>
2025-07-17 13:13:36 -0300

    implements getprofile data and profile form

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -14,6 +14,8 @@ import { useQuery } from '@tanstack/react-query'
 import { getProfile } from './api/get-profile'
 import { getManagedRestaurant } from './api/get-manage-restaurant'
 import { Skeleton } from './ui/skeleton'
+import { Dialog, DialogTrigger } from './ui/dialog'
+import { StoreProfileDialog } from './store-profile-dialog'
 
 export function AccountMenu() {
 
@@ -28,54 +30,60 @@ export function AccountMenu() {
   })
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="flex items-center gap-2 select-none"
-        >
-          {isLoadingManagedRestaurant ? (
-            <Skeleton className='h-4 w-40' />
-          ) : (
-            managedRestaurant?.name
-          )}
-          <ChevronDown className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          {isLoadingProfile ? (
-            <div className='space-y-1.5'>
-              <Skeleton className='h-4 w-32' />
-              <Skeleton className='h-3 w-24' />
-            </div>
-          ) : (
-            <>
-              <span>{profile?.name}</span>
-              <span className="text-muted-foreground text-xs font-normal">
-                {profile?.email}
-              </span>
-            </>
-          )}
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-
-        <DropdownMenuItem>
-          <Building className="mr-2 h-4 w-4" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
-
-        <DropdownMenuItem>
-          <LogOut className="text-rose-500 dark:text-rose-400" />
-          <Link
-            to="/sign-in"
-            className="w-full text-rose-500 dark:text-rose-400"
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex items-center gap-2 select-none"
           >
-            <span>Sair</span>
-          </Link>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className='h-4 w-40' />
+            ) : (
+              managedRestaurant?.name
+            )}
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className='space-y-1.5'>
+                <Skeleton className='h-4 w-32' />
+                <Skeleton className='h-3 w-24' />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-muted-foreground text-xs font-normal">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <DialogTrigger asChild>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
+
+          <DropdownMenuItem>
+            <LogOut className="text-rose-500 dark:text-rose-400" />
+            <Link
+              to="/sign-in"
+              className="w-full text-rose-500 dark:text-rose-400"
+            >
+              <span>Sair</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog/>
+    </Dialog>
   )
 }
--- src/components/api/get-manage-restaurant.ts
+++ src/components/api/get-manage-restaurant.ts
@@ -3,11 +3,10 @@ import { api } from "@/lib/axios";
 interface GetManagedRestaurantResponse {
   id: string;
   name: string;
-  email: string;
-  phone: string | null;
-  role: "manager" | "customer";
   createdAt: Date | null;
   updatedAt: Date | null;
+  description: string | null;
+  managerId: string | null;
 }
 
 export async function getManagedRestaurant() {
